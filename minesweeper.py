aaa5f23c38e460fefac4ad4058764519a0cbce38
ntruter42 <ntruter42@gmail.com>
2021-03-20 00:49:26 +0200

    Updated minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,12 @@
 from os import system
 
 def createBoard(size, bombs):
-	board = []
 	row = []
+	board = []
 
-	for rows in range(0, size):
-		for cols in range(0, size):
-			row.append(" ")
+	for x in range(0, size):
+		row.append(" ")
+	for y in range(0, size):
 		board.append(row)
 
 	return(board)
@@ -17,19 +17,46 @@ def createBoard(size, bombs):
 
 def printBoard(board):
 	system("cls||clear")
-	output_rows = len(board) + 3
-	output_cols = (len(board[0]) * 2) + 2
-	output = " "
+	size = len(board)
+	output_rows = size + 3
+	output_cols = (size * 2) + 2
+
+	x_pos = 0
+	y_pos = 0
+
 	counter = 0
 
-	for y in range(0, output_cols):
-		for x in range(0, output_rows):
+	extra_space = 0
+	if size > 10:
+		extra_space += 1
+
+	for y in range(0, output_rows):
+		output = ""
+		x_pos = 0
+		for x in range(0, output_cols):
 			if y == 0:
-				if x == 0 or x == 1 or x % 2 == 1:
+				if x == 0 or x % 2 == 1:
 					output += " "
-				elif x % 2 == 0:
+				else:
 					output += str(counter)
 					counter += 1
+			elif y == 1 or y == output_rows - 1:
+				output += "-"
+				counter = 0
+			elif y > 1 and y < output_rows:
+				if x == 0:
+					if size > 10 and counter < 10:
+						output += " "
+					output += str(counter)
+				elif x % 2 == 1:
+					output += "|"
+				else:
+					output += board[y_pos][x_pos]
+					x_pos += 1
+					if x_pos == len(board):
+						y_pos += 1
+						counter += 1
+		print(output)
 
 
 def main():
